f9b31693aa171c773f07bfa48b893aa9bed38e03
Boris Khalkov <boris.khalkov@gmail.com>
2023-01-18 12:19:43 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-main>
+      <ContentBlock/>
+    </v-main>
+
+    <v-footer app v-bind="localAttrs">
+      <FooterVue />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import ContentBlock from './components/ContentBlock';
+import FooterVue from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components: {
+    ContentBlock,
+    FooterVue
+  },
+
+  computed: {
+    localAttrs () {
+      const attrs = {}
+
+      attrs.absolute = true
+      attrs.fixed = false
+
+      return attrs
+    },
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
